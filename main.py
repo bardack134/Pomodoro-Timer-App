78249d7adf6371ad2ccc60b6c4b80175623f7f66
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-03-08 08:02:33 +0900

    metodo pomodoro funcionando

--- main.py
+++ main.py
@@ -16,14 +16,32 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+#variable global que almacena la repeticion actual del pomodoro
+reps=1
 
+#funcion que se ejecutara despues de oprimir el boton start
+def start_pomodoro():
+    global reps #para poder trabajar con variables globales
+    work_seconds =WORK_MIN*60
+    
+    short_break_seconds = SHORT_BREAK_MIN*60
+    
+    long_break_seconds = LONG_BREAK_MIN*60
+    
+    if reps==1 or reps==3 or reps==5 or reps==7:
+        countdown(work_seconds)
+    elif reps==2 or reps==4 or reps==6:
+        countdown(short_break_seconds)
+    elif reps==8:
+        countdown(long_break_seconds)
 
 
 #TODO: FUNCION DE CUENTA REGRESIVA DESDE 25 MINUTOS
-#funciona llamar despues de el tiempo especificado en widnw.after()
 def countdown(count_seconds):
+    global reps
+    
     # el cociente sería el número de minutos completos y el residuo sería el número de segundos restantes
-    seconds = (count_seconds%60)    
+    seconds = count_seconds%60    
        
     #1 minuto tiene 60 segundos, divido los segundos en 60 para saber cuantos minutos hay      
     minutes=math.floor(count_seconds/60)
@@ -31,15 +49,23 @@ def countdown(count_seconds):
     if seconds == 0:
         seconds="00"
         canvas.itemconfig(timer_text, text=(f"{minutes}:{seconds}"))
+        
+    elif seconds < 10:
+        seconds=(f"0{seconds}")
+        canvas.itemconfig(timer_text, text=(f"{minutes}:{seconds}"))
     
     #itemconfig ,metodo para cambiar la configuracion de un elemento de canvas, especificando el elemento que se desa cambiar, en este caso
     #deseo cambiar el texto del elemento canvas timer_text
     canvas.itemconfig(timer_text, text=(f"{minutes}:{seconds}"))
     
+    #para evitar numeros negativos en nuestra pantalla
     if count_seconds >0:
+        # after() se utiliza para programar una función para que se ejecute después de un número específico de milisegundos
         window.after(1000, countdown, count_seconds-1)
     
-
+    if minutes == 0 and count_seconds==0:
+        reps+=1
+        start_pomodoro()
 # ---------------------------- UI SETUP ------------------------------- #
 #TODO: CONFIGURACION GENERAL DE LA VENTANA
 # create a tkinter window
@@ -77,7 +103,7 @@ timer_label.grid(row=0, column=1, pady=2)
 #boton start
 #convirtiendo el tiempo total de trabajo en minutos a segundos
 
-start_button=Button(text="start", relief="groove", bg="white", command=lambda:countdown(WORK_MIN*60))
+start_button=Button(text="start", relief="groove", bg="white", command=start_pomodoro)
 start_button.grid(row=2, column=0)
 
 #boton reset
